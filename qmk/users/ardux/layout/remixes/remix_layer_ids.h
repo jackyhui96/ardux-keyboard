1e4cf0ecddcb3a94a1573009a51e9c927f5edbec
Jacky Hui <github@halojax.anonaddy.com>
2022-07-17 11:09:36 +0100

    Renamed files from artsey to ardux

--- qmk/users/ardux/layout/remixes/remix_layer_ids.h
+++ qmk/users/ardux/layout/remixes/remix_layer_ids.h
@@ -3,4 +3,8 @@
 #define LAYER_ID_40P_SYM (LAYER_ID_BIG_FUN + 2)
 #define LAYER_ID_40P_NUM (LAYER_ID_BIG_FUN + 3)
 #define LAYER_ID_40P_MOUSE (LAYER_ID_BIG_FUN + 4)
-#define LAYER_ID_40P_BUTTON (LAYER_ID_BIG_FUN + 5)
+
+// Use Button rather than Big Fun as the last layer (max 16 layers)
+#define LAYER_ID_40P_BUTTON (LAYER_ID_BIG_FUN)
+#undef LAYER_ID_BIG_FUN
+#define LAYER_ID_BIG_FUN 16
